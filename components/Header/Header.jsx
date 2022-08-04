f06e869dc4dc6699bfe55b5ca73af81caf82ca68
Amirhan Inasalamov <amirhan.inasalamov@mail.ru>
2022-08-04 12:02:59 +0300

    Добавил HomePage и папку

---
+++ components/Header/Header.jsx
@@ -0,0 +1,84 @@
+import styles from "./Header.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BsPersonCircle } from "react-icons/bs"
+
+
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.Header}>
+      <div className={styles.logo_container}>
+        <img
+          src={
+            "https://lasvegas.wpresidence.net/wp-content/uploads/2022/03/las-vegas.png"
+          }
+          alt="logo"
+        ></img>
+      </div>
+      <div className={styles.list_container}>
+        <ul className={styles.list}>
+          <li
+            className={
+              router.pathname == "/"
+                ? styles.active_link
+                : styles.disactive_link
+            }
+          >
+            <Link href="/">Home</Link>
+          </li>
+
+          <li
+            className={
+              router.pathname == "/apartments"
+                ? styles.active_link
+                : styles.disactive_link
+            }
+          >
+            <Link href="/apartments">Apartments</Link>
+          </li>
+          <li
+            className={
+              router.pathname == "/agents"
+                ? styles.active_link
+                : styles.disactive_link
+            }
+          >
+            <Link href="/agents">Agents</Link>
+          </li>
+          <li
+            className={
+              router.pathname == "/contact"
+                ? styles.active_link
+                : styles.disactive_link
+            }
+          >
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li
+            className={
+              router.pathname == "/about"
+                ? styles.active_link
+                : styles.disactive_link
+            }
+          >
+            <Link href="/about">About us</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.acc_container}>
+        <span className={styles.phone_icon}>
+          <BsFillTelephoneFill />
+        </span>
+        <span className={styles.phone_number}>8-800-555-35-35</span>
+        <span className={styles.acc_icon}>
+          <BsPersonCircle />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header/Header";
+import SearchContent from "../components/SearchContent/SearchContent";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <h1>Hello world!</h1>
+  return (
+    <div>
+      <Header />
+      <SearchContent />
+    </div>
+  );
 }
