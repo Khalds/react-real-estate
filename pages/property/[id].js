c670c462d31f84ef1eeb89662cc07c5116f35da8
Zelim <anzorov.zelim@gmail.com>
2022-08-11 12:27:54 +0300

    немного изменил стили в property photo gallery

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,12 +1,12 @@
-import Title from "../components/Property/Title";
-import Overview from "../components/Property/Overview";
-import Description from "../components/Property/Description";
-import Address from "../components/Property/Address";
-import Calculator from "../components/Property/Calculator";
-import Showing from "../components/Property/Showing";
-import PhotoGallery from "../components/Property/PhotoGallery/PhotoGallery";
+import Title from "../../components/Property/Title";
+import Overview from "../../components/Property/Overview";
+import Description from "../../components/Property/Description";
+import Address from "../../components/Property/Address";
+import Calculator from "../../components/Property/Calculator";
+import Showing from "../../components/Property/Showing";
+import PhotoGallery from "../../components/Property/PhotoGallery/PhotoGallery";
 
-import styles from "../components/Property/Property.module.css";
+import styles from "../../components/Property/Property.module.css";
 
 
 const property = () => {
